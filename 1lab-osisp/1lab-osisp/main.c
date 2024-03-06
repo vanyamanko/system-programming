1bf6403bb1f422d2b8c766b4f5d67fc4868b3899
vanyamanko <ivanmanko04@icloud.com>
2024-03-06 22:57:58 +0300

    add find by name

--- 1lab-osisp/1lab-osisp/main.c
+++ 1lab-osisp/1lab-osisp/main.c
@@ -10,11 +10,12 @@ struct flags
     int d;
     int f;
     int s;
+    int name;
 };
 
-struct flags find_flags_and_path(int argc, char* argv[], char* path)
+struct flags find_flags_and_path(int argc, char* argv[], char* path, char* name)
 {
-    struct flags flag = { 0, 0, 0, 0 };
+    struct flags flag = { 0, 0, 0, 0, 0};
     for(int j = 1; j < argc; j++)
     {
         if(argv[j][0] == '-')
@@ -22,6 +23,7 @@ struct flags find_flags_and_path(int argc, char* argv[], char* path)
             
             for(size_t i = 0; i < strlen(argv[j]); i++)
             {
+                if(argv[j][i] == 'n') flag.name = 1;
                 if(argv[j][i] == 'l') flag.l = 1;
                 if(argv[j][i] == 'd') flag.d = 1;
                 if(argv[j][i] == 'f') flag.f = 1;
@@ -29,6 +31,10 @@ struct flags find_flags_and_path(int argc, char* argv[], char* path)
             }
         }
         if(argv[j][0] == '/' || argv[j][0] == '.') strcpy(path, argv[j]);
+        if (argv[j][0] == '%' && argv[j][strlen(argv[j]) - 1] == '%') {
+            strcpy(name, argv[j]);
+           // printf("1\n");
+        }
     }
     if(!flag.l && !flag.d && !flag.f)
     {
@@ -43,24 +49,67 @@ int compare(const struct dirent **a, const struct dirent **b) {
     setlocale(LC_COLLATE, "");
     return strcoll((*a)->d_name, (*b)->d_name);
 }
+
+void free_file_list(struct dirent** file_list, int file_count) {
+    for (int i = 0; i < file_count; i++) {
+        free(file_list[i]);
+    }
+    free(file_list);
+}
+void check_flag_name(int flag, char file[256], char name[256], char path[1024])
+{
+    if((flag > 1 && !strcmp(file, name)) || flag < 2)
+    {
+        printf("%s/%s\n", path, file);
+    }
+}
+void dirwalk(char path[1024], struct flags flag, char* name)
+{
+    struct dirent** file_list = NULL;
+
+    int count = scandir(path, &file_list, NULL, NULL);
+    
+    if(strlen(name) != 0 && flag.name)
+    {
+        flag.name++;
+    }
+    if(count == -1)
+    {
+        printf("Try again!\n");
+        return;
+    }
+    if(flag.s) count = scandir(path, &file_list, NULL, compare);
+    for (int i = 0; i < count; i++)
+    {
+        if (strcmp(file_list[i]->d_name, ".") == 0 || strcmp(file_list[i]->d_name, "..") == 0) continue;
+        
+        if(flag.d && file_list[i]->d_type == DT_DIR)
+        {
+            check_flag_name(flag.name, file_list[i]->d_name, name, path);
+        }
+        
+        if(file_list[i]->d_type == DT_DIR)
+        {
+            char next_path[1024];
+            strcpy(next_path, path);
+            strcat(next_path, "/");
+            strcat(next_path, file_list[i]->d_name);
+            dirwalk(next_path ,flag, name);
+        }
+        if(flag.f && file_list[i]->d_type == DT_REG) check_flag_name(flag.name, file_list[i]->d_name, name, path);
+        if(flag.l && file_list[i]->d_type == DT_LNK) check_flag_name(flag.name, file_list[i]->d_name, name, path);
+    }
+}
+
 int main(int argc, char* argv[])
 {
-    char path[256] = "./";
-    struct flags flag = find_flags_and_path(argc, argv, path);
-    
-    struct dirent** file_list;
-    
-    int n = scandir(path, &file_list, NULL, NULL);
-    
-    if(flag.s) n = scandir(path, &file_list, NULL, compare);
-    
-    for (int i = 0; i < n; i++)
-    {
-        if(flag.d && file_list[i]->d_type == DT_DIR) printf("%s/%s\n", path, file_list[i]->d_name);
-        if(flag.f && file_list[i]->d_type == DT_REG) printf("%s/%s\n", path, file_list[i]->d_name);
-        if(flag.l && file_list[i]->d_type == DT_LNK) printf("%s/%s\n", path, file_list[i]->d_name);
+    char path[1024] = "./";
+    char name[256] = "";
+    struct flags flag = find_flags_and_path(argc, argv, path, name);
+    if (strlen(name) >= 2) {
+        memmove(name, name + 1, strlen(name) - 2);
+        name[strlen(name) - 2] = '\0';
     }
-    if(n == -1) printf("Try again!\n");
-    
+    dirwalk(path, flag, name);
     return 0;
 }
--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 ### 1-lab
 
-Dirwalk program that scans system files and outputs to stdout
+Dirwalk program scans the file system and outputs to stdout
 information in accordance with the program options.
+The output format is similar to that of the find utility.
