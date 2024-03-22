cf3a2fabcf478e5a2ab8be4e3fd596e83ec5a0d4
vanyamanko <ivanmanko04@icloud.com>
2024-03-22 03:12:46 +0300

    lab2 child prog done

--- 2lab-osisp/parent/parent/main.c
+++ 2lab-osisp/parent/parent/main.c
@@ -1,14 +1,43 @@
-//
-//  main.c
-//  parent
-//
-//  Created by Vanya Manko on 21.03.24.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <unistd.h>
+
+extern char **environ;
+
+int compare_env_vars(const void *a, const void *b) {
+    const char *var_one = *(const char **)a;
+    const char *var_two = *(const char **)b;
+    return strcoll(var_one, var_two);
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    setlocale(LC_COLLATE, "C");
+    
+    int env_count = 0;
+    char **env_vars = NULL;
+    for (char **env = environ; *env != NULL; env++) {
+        env_vars = realloc(env_vars, (env_count + 1) * sizeof(char *));
+        env_vars[env_count] = *env;
+        env_count++;
+    }
+    qsort(env_vars, env_count, sizeof(char *), compare_env_vars);
+    for (int i = 0; i < env_count; i++) {
+        printf("%s\n", env_vars[i]);
+    }
+    
+    while (1) {
+        int c = getchar();
+        
+        if(c == '+') {
+            //child
+        }
+        
+        if (c == 'q') {
+            break;
+        }
+    }
+    
     return 0;
 }
