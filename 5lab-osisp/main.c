1ef1d81067e08dd5dcb14b7574f7a5431ed7e601
vanyamanko <ivanmanko04@icloud.com>
2024-05-04 19:46:05 +0300

    5lab done 5.1

---
+++ 5lab-osisp/main.c
@@ -0,0 +1,70 @@
+#include "func.h"
+
+int main(void) {
+    queueSize = START_QUEUE_SIZE;
+    MessageQueue* queue = malloc(sizeof(MessageQueue));
+    dispatch_semaphore_t sem_prod = dispatch_semaphore_create(MAX_SEM_COUNT);
+    dispatch_semaphore_t sem_con = dispatch_semaphore_create(MAX_SEM_COUNT);
+ 
+    static int producerCount = 0;
+    static int consumerCount = 0; 
+
+    while(1) {
+        char c = getchar();
+        if(c == '1') {
+            threadProdFlag = 1;
+            ThreadArgs* argsProd = malloc(sizeof(ThreadArgs));
+            argsProd->queue = queue;
+            argsProd->sem = &sem_prod;
+            pthread_t thread;
+            int result = pthread_create(&thread, NULL, threadMessageProducer, (void*)argsProd);
+            if (result != 0) {
+                perror("pthread_create");
+                exit(EXIT_FAILURE);
+            }
+            producerCount++;
+        }
+        if(c == '2') {
+            threadProdFlag = 0;
+            producerCount = 0;
+        }
+        if(c == '3') {
+            threadConFlag = 1;
+            ThreadArgs* argsCon = malloc(sizeof(ThreadArgs));
+            argsCon->queue = queue;
+            argsCon->sem = &sem_con;
+            pthread_t thread;
+            int result = pthread_create(&thread, NULL, threadMessageConsumer, (void*)argsCon);
+            if (result != 0) {
+                perror("pthread_create");
+                exit(EXIT_FAILURE);
+            }
+            consumerCount++;
+        }
+        if(c == '4') {
+            threadConFlag = 0;
+            consumerCount = 0;
+        }
+        if(c == 's') {
+            printf("Count of producter threads: %d\n", producerCount);
+            printf("Count of consumer threads: %d\n", consumerCount);
+            printf("Queue size: %d\n", queueSize); 
+        }
+        if(c == 'q') {
+            threadProdFlag = 0;
+            threadConFlag = 0;
+            break;
+        }
+        if(c == '+') {
+            queueSize++;
+        }
+        if(c == '-') {
+            if ((queue->tail + 1) % (queueSize + 1) == queue->head) {
+                pop(queue);
+            }
+            queueSize--;
+        }
+    }
+    free(queue);
+    return 0;
+}
